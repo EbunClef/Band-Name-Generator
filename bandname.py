20d3cea26e836e1c09d25c7147b55c58905a188f
EbunClef <64669789+EbunClef@users.noreply.github.com>
2023-04-20 14:08:32 +0100

    Band Name Generator

---
+++ bandname.py
@@ -0,0 +1,11 @@
+#Create a greeting for your program.
+#Ask the user for the city that they grew up in.
+#Ask the user for the name of a pet.
+#Combine the name of their city and pet and show them their band name.
+# print("Hello " + input("What's your name? "))
+# print("The name your band is " + input("Where do you live? ")+input("What's the name of your pet? "))
+
+print("Welcome to the Band Name Generator \n")
+county = input("Where do you live? \n")
+pet = input("What's the name of your pet? \n")
+print("The name of your band is " + county + " " + pet )
